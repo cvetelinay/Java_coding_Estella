8b93858437723fb698409ac760c3340ed69627cf
leena <leena@cybertekschool.com>
2023-12-11 20:13:08 +0300

    Week 9 tasks are added

---
+++ src/week_10/Question2/Leena.java
@@ -0,0 +1,12 @@
+package week_10.Question2;
+
+import java.util.ArrayList;
+
+public class Leena {
+
+/*
+    ArrayList - sorting in descending order
+    Write a method that can sort the ArrayList in descending order without using the sort method.
+ */
+
+}
